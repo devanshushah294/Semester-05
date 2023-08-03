16b937c94ea629f3e2115a52492f2b70c9bcf8b1
Devanshu Shah <93487920+devanshushah294@users.noreply.github.com>
2023-08-03 21:31:56 +0530

    .

---
+++ .NET/MyFirstApplication/MyFirstApplication/Areas/Validation/Controllers/ValidationController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstApplication.Areas.Validation.Controllers
+{
+    [Area("Validation")]
+    public class ValidationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ .NET/MyFirstApplication/MyFirstApplication/Areas/Validation/Models/ValidationModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyFirstApplication.Areas.Validation.Models
+{
+    public class ValidationModel
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string EmailAddress { get; set; } = string.Empty;
+        
+        [Required]
+        [PasswordPropertyText]
+        public string Password { get; set; } = string.Empty;
+        
+        [Required]
+        public string PhoneNumber { get; set; } = string.Empty;
+    }
+}
